0240d63d727d3a37fe80596c9cc9a1811080489c
Rafael Pérez <rafaelsaxo12@gmail.com>
2024-06-11 01:54:05 -0400

    arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,11 @@ function App() {
       <h2>Loading...</h2>
       :
       <>
+      <div className='app__banner'>
         <figure className='app__img'>
           <img src="https://s3-alpha-sig.figma.com/img/2838/9946/263f7612d0101b6aa67c8b9f233f19fb?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ik09mT7QHBHnqwmabvHEwaqxn182JTEuKIlYkmaCNG5HXWu0SkHmf8-ltiFRH4yj2gZeWRxQOQj~UeLR6DHBnyls6jlEf9fZQX0p-rJ0Dlw3grsfIOVf8Z7x~BRaaG6qWNix4wYkSI-YXQTCJky-~2viIi35J8P6LR52v34YGhL61IObeJPGFRGjBFpGw6UK-uYAKSSJqAUa6T-cBoPuGoDuoO-LMRmsnRekX7DY8lpQZRJ-m-vJoLw33BVdHZM~YIwjq7Hugco5YckL3d2f1dsmQMzKC8wM3TVustD7hp1McrKuOlwXtIZp2j6~k7lDDlKSE~L-aM3X~mN6O2Mr0Q__"  width="900" height="200" alt="RickAndMorty image" />
         </figure>
+      </div>
         <form className='app__form' onSubmit={handleSumit}>
           <input className='app__form-input' ref={textInput} type="number" />
           <button className='app__form-btn'>Search</button>
